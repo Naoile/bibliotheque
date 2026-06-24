5dc9fbc059e47ea22534eba8c6d19f335e9d97eb
Naoile <naoile.laghmara@gmail.com>
2026-06-24 12:51:20 +0200

    feat: add home and book-list page components

--- src/app/pages/book-list/book-list.component.ts
+++ src/app/pages/book-list/book-list.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../core/models/book';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
-  templateUrl: './book-list.component.html',
-  styleUrl: './book-list.component.css'
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './book-list.component.html'
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  books: Book[] = [];
+  newTitle = '';
+  newAuthor = '';
+  newGenre = '';
+  newYear?: number;
+  newNotes = '';
 
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.books = this.bookService.getBooks();
+  }
+
+  addBook(): void {
+    if (!this.newTitle || !this.newAuthor) return;
+    this.bookService.addBook({
+      title: this.newTitle,
+      author: this.newAuthor,
+      genre: this.newGenre,
+      year: this.newYear,
+      notes: this.newNotes,
+      read: false
+    });
+    this.books = this.bookService.getBooks();
+    this.newTitle = '';
+    this.newAuthor = '';
+    this.newGenre = '';
+    this.newYear = undefined;
+    this.newNotes = '';
+  }
+
+  deleteBook(id: string): void {
+    this.bookService.deleteBook(id);
+    this.books = this.bookService.getBooks();
+  }
+
+  toggleRead(id: string): void {
+    this.bookService.toggleRead(id);
+    this.books = this.bookService.getBooks();
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { BookService } from '../../services/book.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  imports: [RouterLink],
+  templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  totalBooks = 0;
+  readBooks = 0;
+  toReadBooks = 0;
 
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    const books = this.bookService.getBooks();
+    this.totalBooks = books.length;
+    this.readBooks = books.filter(b => b.read).length;
+    this.toReadBooks = books.filter(b => !b.read).length;
+  }
 }
