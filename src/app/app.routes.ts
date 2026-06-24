e874de489affa415587c5204088ea74b38cb1f43
Naoile <naoile.laghmara@gmail.com>
2026-06-24 15:36:48 +0200

    feat: configure routing for home and book-list pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BookListComponent } from './pages/book-list/book-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'books', component: BookListComponent },
+  { path: '**', redirectTo: '' }
+];
